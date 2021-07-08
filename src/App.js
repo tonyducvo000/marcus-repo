d35df26c1dbfd46acc72f0518dd077aa6e9250dc
Apclassic <brodeybriggs@hotmail.com>
2021-07-07 22:42:52 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router, Route, Switch} from 'reat-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import reactDom from 'react-dom';
 
