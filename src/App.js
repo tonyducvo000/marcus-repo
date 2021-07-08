76f83acf99edafa309aed82808b2dcafe12a724d
Apclassic <brodeybriggs@hotmail.com>
2021-07-07 22:37:07 -0500

    Sorting JS for pathways

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Route, Switch} from 'reat-router-dom';
 
 import reactDom from 'react-dom';
 
 function App() {
   return (
     <React.Fragment> 
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route  component={404} />
+
+        </Switch>
+
+      </Router>
 
     </React.Fragment>    
   );
