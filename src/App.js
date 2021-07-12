813d0b220c1aba8a2bd72ce7963e1bd07773c6fd
Apclassic <brodeybriggs@hotmail.com>
2021-07-11 23:31:11 -0500

    Set up path ways for the home page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
-
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
 class App extends Component{
   render(){
     return(
       <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/" component={Home} />
+            <Route path="/" component={Home} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
 
       </React.Fragment>
     )
